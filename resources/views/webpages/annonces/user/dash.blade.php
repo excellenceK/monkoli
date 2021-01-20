6ba92b2f420f1e84ecd80f7eb160ea14aeb8e1e7
excellenceK <thierrykouamek1@gmail.com>
2021-01-20 18:22:40 +0000

    mon espace backend

--- resources/views/webpages/annonces/user/dash.blade.php
+++ resources/views/webpages/annonces/user/dash.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Lumino - Dashboard</title>
+	<title>MonKoli - Dashboard</title>
 	<link href="{{ asset('css-dash/bootstrap.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('css-dash/font-awesome.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('css-dash/datepicker3.css') }}" rel="stylesheet">
@@ -81,23 +81,45 @@
 		</div>
         <br><br>
         @php
-            $annonce = DB::table('annonces')
+            $annonceColi = DB::table('annonces')
                         ->where('user_id', Auth::user()->id)
                         ->join('transport_colis', 'transport_colis.annonce_id', 'annonces.id')
+                        ->whereDate('dateExpiration', '>', \Carbon\Carbon::now())
+                        ->select('annonces.id as idAnnonce', 'annonces.*', 'transport_colis.*')
+                        ->get();
+            $annonceColiTotal = DB::table('annonces')
+                        ->where('user_id', Auth::user()->id)
+                        ->join('transport_colis', 'transport_colis.annonce_id', 'annonces.id')
+                        //->whereDate('dateExpiration', '>', \Carbon\Carbon::now())
+                        ->select('annonces.id as idAnnonce', 'annonces.*', 'transport_colis.*')
+                        ->get();
+            $totalColis = $annonceColiTotal->count();
+            //dd($totalColis);
+            $annonceVehicule = DB::table('annonces')
+                        ->where('user_id', Auth::user()->id)
+                        //->join('transport_colis', 'transport_colis.annonce_id', 'annonces.id')
                         ->join('location_vehicules', 'location_vehicules.annonce_id', 'annonces.id')
+                        //->join('location_appartements', 'location_appartements.annonce_id', 'annonces.id')
+                        ->select('annonces.id as idAnnonce', 'annonces.*', 'location_vehicules.*')
+                        ->get();
+            $annonceMaison = DB::table('annonces')
+                        ->where('user_id', Auth::user()->id)
+                       // ->join('transport_colis', 'transport_colis.annonce_id', 'annonces.id')
+                       // ->join('location_vehicules', 'location_vehicules.annonce_id', 'annonces.id')
                         ->join('location_appartements', 'location_appartements.annonce_id', 'annonces.id')
-                        ->select('annonces.id as idAnnonce', 'annonces.*', 'transport_colis.*', 'location_vehicules.*', 'location_appartements.*')
+                        ->select('annonces.id as idAnnonce', 'annonces.*', 'location_appartements.*')
                         ->get();
 
-            //Compte Reservation
-            $reservations = 0;
-            foreach ($annonce as  $value) {
-                # code...
-                $reservations += DB::table('reservations')->where('annonce_id', $value->idAnnonce)->count();
 
+            //dd($annonceColi);
+            //Compte Reservation
+            $reservationsColi = 0;
+            foreach ($annonceColi as  $value) {
+                # code...
+                $reservationsColi += DB::table('reservations')->where('annonce_id', $value->idAnnonce)->count();
 
             }
-            $annonceEnCours = $annonce->count();
+            $annonceEnCours = $annonceColi->count();
             //dd($reservations);
 
         @endphp
@@ -116,7 +138,7 @@
 					<div class="panel panel-teal panel-widget border-right">
 						<div class="row no-padding">
 							<div class="text-muted" style="font-size: 15px;">Reservation(s) en attente<br/></div>
-							<div class="large" style="color: #00E38C;">{{ $reservations }}</div>
+							<div class="large" style="color: #00E38C;">{{ $reservationsColi }}</div>
 							<button type="button" class="btn pure2 offset-lg-2 offset-md-2" style="background-color: #00E38C; color: white; width: 160px; height: 40px";> <i style="color: white; opacity: 0.7;" class="fas fa-eye" aria-hidden="true"></i>  Consultez les</button>
 						</div>
 					</div>
@@ -125,7 +147,7 @@
 					<div class="panel panel-teal panel-widget border-right">
 						<div class="row no-padding">
 							<div class="text-muted" style="font-size: 15px;">Total Annonces<br/></div>
-							<div class="large" style="color: #00E38C;">{{ $annonceEnCours }}</div>
+							<div class="large" style="color: #00E38C;">{{ $totalColis }}</div>
 							<button type="button" class="btn pure2 offset-lg-2 offset-md-2" style="background-color: #00E38C; color: white; width: 160px; height: 40px";><i style="color: white; opacity: 0.7;" class="fas fa-eye" aria-hidden="true"></i>  Consultez les</button>
 						</div>
 					</div>
@@ -147,14 +169,32 @@
 				<h3 class="page-header" style="font-weight: bold;">Réservations / trajet</h3>
 				<div class="panel panel-default" style="box-shadow: 0 0px 26px 5px #C6C2C2;">
 					<div class="panel-body">
-						<div class="row progress-labels">
-						<div class="col-sm-6"><span>Abidjan </span><i class="fas fa-arrow-right"></i><span> Paris</span><br><br></div>
-							<div class="col-sm-6" style="text-align: right;">25%</div>
-						</div>
-						<div class="progress">
-							<div data-percentage="0%" style="width: 25%;" class="progress-bar progress-bar-blue" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
-						</div>
-						<div class="row progress-labels">
+                        @foreach($annonceColiTotal as $value)
+                            @php
+                                $reservations = DB::table('reservations')->where('annonce_id', $value->idAnnonce)->get();
+                                $quantiteReserve = 0;
+                                $totalGain = 0;
+                                $pourcentageReservation = 0;
+                                foreach ($reservations as $data) {
+                                    # code...
+                                    $quantiteReserve += $data->quantiteReserve;
+                                    $totalGain += $data->montantReservation;
+                                }
+                                $pourcentageReservation = ($quantiteReserve * 100)/$value->quantiteDisponible;
+                                //dd($reservation);
+                            @endphp
+                            <div class="row progress-labels">
+                                <div class="col-sm-6"><span>{{ $value->villeDepart }} </span><i class="fas fa-arrow-right"></i><span> {{ $value->villeArriver }}</span><br><br></div>
+                                <div class="col-sm-6" style="text-align: right;">{{ $pourcentageReservation }}%</div>
+                            </div>
+                            <div class="progress">
+                                <div data-percentage="0%" style="width: {{ $pourcentageReservation }}%;" class="progress-bar progress-bar-blue" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        @endforeach
+
+
+
+					<!--	<div class="row progress-labels">
 							<div class="col-sm-6"><span>Abidjan </span><i class="fas fa-arrow-right"></i><span> Dakar</span><br><br></div>
 							<div class="col-sm-6" style="text-align: right;">25%</div>
 						</div>
@@ -174,7 +214,7 @@
 						</div>
 						<div class="progress">
 							<div data-percentage="0%" style="width: 25%;" class="progress-bar" style="color: #00E38C" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
-						</div>
+						</div>-->
 					</div>
 
 				</div>
@@ -184,17 +224,31 @@
 				<h3 class="page-header" style="font-weight: bold;">Total Gain</h3>
 				<div class="panel panel-default" style="box-shadow: 0 0px 26px 5px #C6C2C2;">
 					<div class="panel-body">
-						<div class="large" style="color: #00E38C; font-weight: bolder; font-size: 50px;">80 000 <span style="color: #C6C2C2; font-weight: 500;">FCFA</span>
-							<br><br>
-							<div class="col-xs-6 col-md-3 col-lg-3 no-padding" style="background-color: none;">
-								<div class="panel panel-teal panel-widget border-right">
-									<div class="row no-padding">
-										<div class="text-muted" style="font-size: 7px; color: #3C3C3C"><span>Abidjan </span><i class="fas fa-arrow-right" style="font-size: 9px;"></i><span> Paris</span><br/><br></div>
-										<div class="text-muted" style="font-size: 15px; color: #00E38C">20 000<br/></div>
-									</div>
-								</div>
-							</div>
-							<div class="col-xs-6 col-md-3 col-lg-3 no-padding" style="background-color: none;">
+						<div class="large" style="color: #00E38C; font-weight: bolder; font-size: 50px;">{{ $totalGain }} <span style="color: #C6C2C2; font-weight: 500;">FCFA</span>
+                            <br><br>
+
+                            @foreach($annonceColiTotal as $value)
+                                @php
+                                    $reservations = DB::table('reservations')->where('annonce_id', $value->idAnnonce)->get();
+                                    $totalParTrajet = 0;
+                                    foreach ($reservations as $data) {
+                                        # code...
+                                        $totalParTrajet += $data->montantReservation;
+                                    }
+                                //dd($reservation);
+                                @endphp
+                                <div class="col-xs-6 col-md-3 col-lg-3 no-padding" style="background-color: none;">
+                                    <div class="panel panel-teal panel-widget border-right">
+                                        <div class="row no-padding">
+                                            <div class="text-muted" style="font-size: 7px; color: #3C3C3C"><span>{{ $value->villeDepart }} </span><i class="fas fa-arrow-right" style="font-size: 9px;"></i><span> {{ $value->villeArriver }}</span><br/><br></div>
+                                            <div class="text-muted" style="font-size: 15px; color: #00E38C">{{ $totalParTrajet }}<br/></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            @endforeach
+
+							<!--<div class="col-xs-6 col-md-3 col-lg-3 no-padding" style="background-color: none;">
 								<div class="panel panel-teal panel-widget border-right">
 									<div class="row no-padding">
 										<div class="text-muted" style="font-size: 7px; color: #3C3C3C"><span>Abidjan </span><i class="fas fa-arrow-right" style="font-size: 9px;"></i><span> Dakar</span><br/><br></div>
@@ -217,7 +271,7 @@
 										<div class="text-muted" style="font-size: 15px; color: #00E38C">20 000<br/></div>
 									</div>
 								</div>
-							</div>
+							</div>-->
 						</div>
 					</div>
 				</div>
@@ -227,7 +281,7 @@
 		</div><!--/.row-->
 
 
-		<div class="row">
+		<!--<div class="row">
 			<div class="col-sm-6">
 				<h1 class="page-header" style="font-weight: bolder;">Résidences</h1>
 				<h3 class="page-header" style="font-weight: bold;">Chiffres</h3>
@@ -291,7 +345,7 @@
 					</div>
 				</div>
 			</div>
-		</div><!--/.row-->
+		</div> --><!--/.row-->
 
 	</div>
 
